29264638d87d9acfbd4c304712709be8c272f01d
Chris <jlfang07@gamil.com>
2017-09-28 16:29:42 +0800

    Problem 611

---
+++ Leetcode/src/array/ValidTriangleNumber.java
@@ -0,0 +1,44 @@
+package array;
+
+import java.util.Arrays;
+
+
+	/**
+	 * 
+	 * Given an array consists of non-negative integers, 
+	 * your task is to count the number of triplets chosen 
+	 * from the array that can make triangles if we take them as 
+	 * side lengths of a triangle.
+	 *
+	 */
+public class ValidTriangleNumber {
+    public int triangleNumber(int[] nums) {
+        if(nums==null || nums.length ==0)
+            return 0;
+        
+        int count =0;
+        int n = nums.length-1;
+        
+        Arrays.sort(nums);
+        
+        for(int i=n;i>=2;i--)
+        {
+            int low = 0;
+            int high = i-1;
+            
+            while(low<high)
+            {
+                if(nums[low]+nums[high]>nums[i])
+                {
+                    count+=(high-low);
+                    high--;
+                }
+                else
+                    low++;
+
+            }
+        }
+        return count;
+        
+    }
+}
